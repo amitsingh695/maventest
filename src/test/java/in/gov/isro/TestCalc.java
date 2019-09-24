2e5786a3ed90e1ddcd1ba654207f01152a311569
amitsingh695 <55529169+amitsingh695@users.noreply.github.com>
2019-09-24 11:10:28 +0530

    Update TestCalc.java

--- src/test/java/in/gov/isro/TestCalc.java
+++ src/test/java/in/gov/isro/TestCalc.java
@@ -28,7 +28,7 @@ public class TestCalc {
 	public void test3() {
 		Calculator calc = new Calculator();
 		int result = calc.divide(5, 5);
-		assertEquals(1, result);
+		assertEquals(0, result);
 	}
 
 }
